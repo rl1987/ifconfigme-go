c1aae8e9bab0ab7e3c18bd515b19c11034a4e136
rl1987 <rl1987@sdf.lonestar.org>
2017-12-03 13:37:51 +0100

    Show port and HTTP headers

--- main.go
+++ main.go
@@ -17,8 +17,23 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	spew.Dump(r)
 
-	fmt.Fprintln(w, "<html>Hello there!<br>")
-	fmt.Fprintf(w, "IP: %s<br>", strings.Split(r.RemoteAddr, ":")[0])
+	fmt.Fprintln(w, "<html>Hello there!<br><br>")
+
+	if strings.Contains(r.RemoteAddr, ":") {
+		fmt.Fprintf(w, "IP: %s<br>", strings.Split(r.RemoteAddr, ":")[0])
+		fmt.Fprintf(w, "Port: %s<br>", strings.Split(r.RemoteAddr, ":")[1])
+	} else {
+		fmt.Fprintf(w, "RemoteAddr: %s<br>", r.RemoteAddr)
+	}
+
+	fmt.Fprintf(w, "<br>Headers:</br>")
+
+	for h, c := range(r.Header) {
+		for _, cc := range(c) {
+			fmt.Fprintf(w, "%s : %s<br>", h, cc)
+		}
+	}
+
 	fmt.Fprintln(w, "</html>")
 }
 
